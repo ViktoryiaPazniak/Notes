5aa75957db0585698b49bc390f4c458223b15c4e
Viktoryia Pazniak <vikaa.poznyak17@gmail.com>
2024-08-22 23:59:23 +0300

    Added controllers

--- Notes.backend/Notes.WebApi/Program.cs
+++ Notes.backend/Notes.WebApi/Program.cs
@@ -1,5 +1,54 @@
+using Notes.Application.Commom.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Application;
+using Notes.Persistance;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+using (var scope  = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
